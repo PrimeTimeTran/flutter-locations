700e08a6959c706214f22639761e7bb62c2b4f44
Loi Van Tran <datloiboi@gmail.com>
2022-02-22 09:57:07 +0700

    Got ip, geolocation

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:geolocator/geolocator.dart';
+import 'package:dart_ipify/dart_ipify.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class Album {
-  final int userId;
-  final int id;
-  final String title;
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
 
-  const Album({
-    required this.userId,
-    required this.id,
-    required this.title,
-  });
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    print(json);
-    print('Hi After');
-    return Album(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-    );
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
   }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
 }
 
 Future<List> fetchAlbum() async {
   final response =
       await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
     return jsonDecode(response.body);
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
     throw Exception('Failed to load album');
   }
 }
@@ -46,9 +59,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sky Mavis',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Sky Mavis'),
     );
@@ -64,7 +77,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _counter = '';
   late Future<List> futureAlbum;
 
   @override
@@ -73,9 +86,15 @@ class _MyHomePageState extends State<MyHomePage> {
     futureAlbum = fetchAlbum();
   }
 
-  void _incrementCounter() {
+  void _incrementCounter() async {
+    final ipv4 = await Ipify.ipv4();
+    print(ipv4);
+    final mygeo = await Ipify.geo('at_q45RwWwG71w0CvON3ioUJuPwYgm75');
+    print(mygeo.location);
+    var go = await _determinePosition();
+    print(go);
     setState(() {
-      _counter++;
+      _counter = '$go, $ipv4, $mygeo';
     });
   }
 
@@ -83,31 +102,33 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('$_counter'),
       ),
       body: FutureBuilder<List>(
         future: futureAlbum,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data?.length,
-              itemBuilder: (context, index) {
-                var text = snapshot.data?[index]['title'];
-                print(text);
-                return Text("${text}");
-              },
+            return Container(
+              padding: EdgeInsets.symmetric(horizontal: 50.0),
+              child: ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (context, index) {
+                  var text = snapshot.data?[index]['title'];
+                  return Text("${text}");
+                },
+              )
+              ),
             );
           } else if (snapshot.hasError) {
             return Text('${snapshot.error}');
           }
 
-          // By default, show a loading spinner.
           return const CircularProgressIndicator();
         },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment by 1',
+        tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
     );
